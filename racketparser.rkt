9997a49beb3eca1afaab915c19d8070a3f2b668e
inlimbo <rex1324567@gmail.com>
2022-03-06 22:51:24 -0600

    program and stmt_list added

--- racketparser.rkt
+++ racketparser.rkt
@@ -7,18 +7,29 @@
     [else
   (print n)]))
 
-(define parse
-  (let ([source (open-input-file "/Users/Crow/Documents/RacketParser/input.txt")])
-    (let kernel ([token "Accept"])
-    (let ([nextval (read-char source)])
-      (cond
-        [(eof-object? nextval)
-         (close-input-port source)
-         (print token)]
-        [else
-        (dosomething nextval)
-        (kernel token)])))))
-        ;add result of dosomething to kernel
-(define a (file->string "/Users/Crow/Documents/RacketParser/input.txt")
-  (string-split "a b   c\nd" #px"[\\s]+"))
+(define file-string (file->string "/Users/Crow/Documents/RacketParser/input.txt"))
   
+(define file-list (string-split file-string #px"[\\s]+"))
+
+(define (stmt list-number)
+  (print "stmt"))
+
+(define (stmt-list list-number)
+  (cond
+    [(equal? (list-ref file-list list-number) "$$")]
+
+    [else
+     (stmt (+ list-number 1))]))
+
+(define program
+  (cond
+    [(equal? (list-ref file-list 0) "{")
+       (stmt-list 1)]
+    [else
+     (cond
+       [(equal? (length file-list) 0)
+       (print "Empty File")]
+       [else
+        ("Syntax Error")])]))
+
+;(define parse program)
