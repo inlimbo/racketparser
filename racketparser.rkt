09e607004235c541fd44bd31107ef832b0445e31
inlimbo <rex1324567@gmail.com>
2022-03-06 23:41:27 -0600

    stmt added

--- racketparser.rkt
+++ racketparser.rkt
@@ -1,18 +1,25 @@
 #lang racket
 
-(define (dosomething n)
-  (cond
-    [(equal? #\space n)
-     (print "there was a space")]
-    [else
-  (print n)]))
-
 (define file-string (file->string "/Users/Crow/Documents/RacketParser/input.txt"))
   
 (define file-list (string-split file-string #px"[\\s]+"))
 
+(define (expr list-number)
+  (print list-number))
+
 (define (stmt list-number)
-  (print "stmt"))
+  (define list-item (list-ref file-list list-number))
+  (define nextItem (list-ref file-list (+ list-number 1)))
+  (cond
+    [(equal? list-item "read")
+     (regexp-match-exact? #px"[A-z]+" nextItem
+      (equal? (list-ref file-list (+ list-number 2)) ";"
+      (stmt-list (+ list-number 1))
+      (print "Syntax Error"))
+      (print "Syntax Error"))])
+  (cond
+    [(equal? list-item "write")
+     (expr (+ list-number 1))]))
 
 (define (stmt-list list-number)
   (cond
